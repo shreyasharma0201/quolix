bfe777cecdfa33d7584c8666ed7d207346a691a1
Shreya Sharma <shreyashobha0201@gmail.com>
2024-04-01 14:24:31 +0530

    board-list

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,18 +1,22 @@
-import {Info} from "./_components/info"
+import { Suspense } from "react";
+
 import { Separator } from "@/components/ui/separator";
+
+import { Info } from "./_components/info";
 import { BoardList } from "./_components/board-list";
 
 const OrganizationIdPage = async () => {
-
-    return (
-        <div>
-            <Info/>
-            <Separator className="my-4" />
-            <div className="px-2 md:px-4">
-                <BoardList />
-            </div>
-        </div>
-    )
+  return (
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
