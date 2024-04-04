98f2371172fdd3f891d259e63f9f18f2cd1cc3ba
Shreya Sharma <shreyashobha0201@gmail.com>
2024-04-04 23:29:58 +0530

    card-actions

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "bg-neutral-200 text-red-500 hover:bg-neutral-300 hover:text-red-500/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
@@ -19,13 +19,15 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         primary: "bg-sea-green text-primary-foreground hover:bg-sea-green/90",
-        transparent: "bg-transparent text-white hover:bg-white/20"
+        transparent: "bg-transparent text-white hover:bg-white/20",
+        gray: "bg-neutral-200 text-secondary-foreground hover:bg-neutral-300",
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
+        inline: "h-auto px-2 py-1.5 text-sm"
       },
     },
     defaultVariants: {
